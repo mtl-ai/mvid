2f471c12d26bacefd445bdcd9ade40d5aa991f92
Adam Alcolado <adam.alcolado@mtl.ai>
2026-02-27 06:10:34 -0500

    changed order in benchmarking

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -13,19 +13,16 @@ if __name__ == "__main__":
 
     class_list = [AVVideo, Video]
     thread_type_list = ["AUTO", "SLICE", "FRAME"]
-    thread_count_list = [0]  # [0, 1, 5]
+    thread_count_list = [0, 1, 5]
 
-    access_pattern_list = [
-        0,
-        1,
-        5,
-    ]  # number of sequence frames to access before random skip
+    # number of sequence frames to access before random skip
+    access_pattern_list = [0, 1, 5,]
 
     n_frames = 500
 
     tqdm.write(f"Video Path: {video_path}")
-    for t_type, t_count, window, cls in product(
-        thread_type_list, thread_count_list, access_pattern_list, class_list
+    for t_type, t_count, cls, window in product(
+        thread_type_list, thread_count_list, class_list, access_pattern_list
     ):
         with cls(video_path, thread_type=t_type, thread_count=t_count) as video:
             indices = np.arange(len(video))
